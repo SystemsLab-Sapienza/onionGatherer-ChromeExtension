e8514c5301c98213fab5fd8a44a67709b7f476ac
simone <s.raponi.93@gmail.com>
2017-02-22 17:20:16 +0100

    First Commit

---
+++ OnionGatherer.js
@@ -0,0 +1,67 @@
+  var HOST_URL = 'xxxxx'
+
+  var body = document.body;
+  var textContent = body.innerText;
+
+  var split = textContent.split(/[\r\n\t\"\'();\\|<>\s]+/g);
+  var onions = []
+
+  substring = "..."
+
+  for(var i = 0; i < split.length; i++) {
+    // Insert in onions every string that matches with the RegEx
+    if(split[i].match(/^(http(s)?:\/\/)?.{16}(\.onion)\/?.*$/g)) {
+      if(split[i].indexOf(substring) == -1) {
+        onions.push(split[i]);
+      }
+    }
+  }
+
+  var onionsJson = JSON.stringify({onions:onions, website: window.location.href});
+
+  var greenImgURL = chrome.extension.getURL("greenCircle.png");
+  var redImgURL = chrome.extension.getURL("redCircle.png");
+  var orangeImgURL = chrome.extension.getURL("orangeCircle.png");
+
+  $.ajax({
+    type: 'POST',
+    url: HOST_URL,
+    crossDomain: true,
+    data: onionsJson,
+
+    success: function(responseData, textStatus, jqXHR) {
+
+      var list = document.getElementsByTagName('body');
+
+      for(var i = 0; i < list.length; i++) {
+
+        container = list[i];
+
+        // Insert the green, red or orange marker, according to the hidden service's status
+        findAndReplaceDOMText(container, {
+          find: /(http:\/\/)?\b[\w\d]{16}\.onion(\/[\S]*|)/g,
+          replace: function(portion) {
+            var element = document.createElement('span');
+
+              if(responseData['onions'][portion.text] == 1) {
+                element.innerHTML=portion.text + "&nbsp;<img src='" + greenImgURL + "' width=\"10\" height=\"10\" />";
+              }
+              else if(responseData['onions'][portion.text] == -1) {
+                element.innerHTML=portion.text + "&nbsp;<img src='" + redImgURL + "' width=\"10\" height=\"10\" />";
+              }
+              else if(responseData['onions'][portion.text] == 0) {
+                element.innerHTML=portion.text + "&nbsp;<img src='" + orangeImgURL + "' width=\"10\" height=\"10\" />";
+              }
+              else {
+                element.innerHTML=portion.text
+              }
+            return element;
+          }
+        });
+      }
+    },
+
+    error: function (responseData, textStatus, errorThrown) {
+     console.warn(responseData, textStatus, errorThrown);
+    }
+  });
---
+++ README.md
@@ -0,0 +1,32 @@
+**Authors:** *Simone Raponi, Massimo La Morgia*
+
+**Date:**   *February 20, 2017*
+
+# Onion Gatherer Client
+
+Front-end of [insertLink]
+
+Onion Gatherer is a Google Chrome extension that allows Tor users to know the hidden service status without copying and pasting its address from time to time.
+The user, surfing between websites containing the Hidden service addresses, will appear alongside each one a mark that reveals its status according to its color.
+
+ - ![Green mark](figures/greenCircle.png): The hidden service is up
+ - ![Red mark](figures/redCircle.png): The hidden service is down
+ - ![Orange mark](figures/orangeCircle.png): The server is evaluating the Hidden service status
+
+### Requirements
+ - Google Chrome
+
+### Installation
+
+Insert into the variable HOST_URL in the file named OnionGatherer.js the url of your own Host.
+
+Open the Google Chrome extensions page by typing in the address bar
+```sh
+chrome://extensions
+```
+
+Click on the button: 
+
+![Load unpacked extension...](figures/button.png) 
+
+and load the zip project file
